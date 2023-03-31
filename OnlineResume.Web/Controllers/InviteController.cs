d66593e864b0de964d671caca8b303160806c944
JimJay <jmjgeminix@163.com>
2023-03-31 15:55:59 +0800

    temp save

--- OnlineResume.Web/Controllers/InviteController.cs
+++ OnlineResume.Web/Controllers/InviteController.cs
@@ -1,8 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using OnlineResume.Web.ViewModels;
+using System.Security.Claims;
 
 namespace OnlineResume.Web.Controllers;
 
 public class InviteController : Controller
 {
+    [HttpGet]
     public IActionResult Index() => View();
+
+    [HttpPost]
+    public async Task<IActionResult> Index(InviteIndexViewModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(model);
+        }
+        if (model.Code == "123")
+        {
+            var claims = new List<Claim>()
+            {
+                new Claim("Company.Name", "连云港电子口岸")
+            };
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            await HttpContext.SignInAsync(
+                scheme: CookieAuthenticationDefaults.AuthenticationScheme,
+                principal: new ClaimsPrincipal(claimsIdentity));
+
+            return RedirectToAction(actionName: nameof(HomeController.Index), controllerName: "Home");
+        }
+        else
+        {
+            ModelState.AddModelError(nameof(model.Code), "邀请码验证错误");
+        }
+        return View();
+    }
 }
---
+++ OnlineResume.Web/ViewModels/InviteIndexViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineResume.Web.ViewModels;
+
+public class InviteIndexViewModel
+{
+    [Required(ErrorMessage = "必须填写邀请码")]
+    public string Code { get; set; } = string.Empty;
+}
